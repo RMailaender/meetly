728daa413b0791d6f66d075a224d1d282c8218d2
Rene Mailaender <r.89.mailaender@gmail.com>
2025-09-17 17:31:33 +0200

    add participant type

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,55 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {
-  const [nameInput, setNameInput] = useState<string>("");
-  const [names, setNames] = useState<string[]>([]);
+type Participant = {
+  name: string;
+  handRaised: boolean;
+  countTimesHandRaised: number;
+  countTimesSpoken: number;
+};
 
-  const addName = (name: string) => setNames([...names, name]);
+const createParticipant = (
+  name: string,
+  handRaised: boolean = false,
+): Participant => ({
+  name,
+  handRaised,
+  countTimesHandRaised: 0,
+  countTimesSpoken: 0,
+});
+
+function App() {
+  const [nameInputValue, setNameInputValue] = useState<string>("");
+  const [nameInputError, setNameInputError] = useState<
+    "participant-already-exists" | null
+  >(null);
+  const [participants, setParticipants] = useState<Participant[]>([]);
+
+  const addParticipant = (handRaised: boolean) => () => {
+    if (participants.find(({ name }) => name === nameInputValue)) {
+      setNameInputError("participant-already-exists");
+      return;
+    }
+
+    setNameInputValue("");
+    setParticipants([
+      ...participants,
+      createParticipant(nameInputValue, handRaised),
+    ]);
+  };
 
   return (
     <>
-      <input onChange={(e) => setNameInput(e.target.value)}></input>
-      <button onClick={() => addName(nameInput)}>add</button>
+      <form action={addParticipant(true)}>
+        <input
+          value={nameInputValue}
+          onChange={(e) => setNameInputValue(e.target.value)}
+        ></input>
+        <button value="submit">add</button>
+        <label>gnarf</label>
+      </form>
       <ul>
-        {names.map((name) => (
+        {participants.map(({ name }) => (
           <li>{name}</li>
         ))}
       </ul>
