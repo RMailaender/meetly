a075dba7d146697e7560061ddd404f1c4d0ceaee
Rene Mailaender <r.89.mailaender@gmail.com>
2025-09-18 11:54:03 +0200

    raise an lower hand

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,33 @@ const createParticipant = (
 ): Participant => ({
   name,
   handRaised,
-  countTimesHandRaised: 0,
+  countTimesHandRaised: handRaised ? 1 : 0,
   countTimesSpoken: 0,
 });
 
+const SpeakersList: React.FunctionComponent<{
+  speakers: Participant[];
+  onLowerHand: (speakerName: string) => void;
+}> = ({ speakers, onLowerHand }) => {
+  if (speakers.length === 0) {
+    return null;
+  }
+
+  return (
+    <>
+      <ul>
+        {speakers.map(({ name }) => (
+          <li key={name} onClick={() => onLowerHand(name)}>
+            {name}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
 function App() {
+  // TODO: Enter Key should create Participant too
   const [nameInputValue, setNameInputValue] = useState<string>("");
   const [nameInputError, setNameInputError] = useState<
     "participant-already-exists" | null
@@ -38,20 +60,54 @@ function App() {
     ]);
   };
 
+  const raiseHand = (participantName: string) =>
+    setParticipants(
+      participants.map((current) =>
+        current.name === participantName
+          ? { ...current, handRaised: true }
+          : current,
+      ),
+    );
+
+  const lowerHand = (participantName: string) =>
+    setParticipants(
+      participants.map((current) =>
+        current.name === participantName
+          ? { ...current, handRaised: false }
+          : current,
+      ),
+    );
+
+  const onNameInputChange = (value: string) => {
+    setNameInputError(null);
+    setNameInputValue(value);
+  };
+
   return (
     <>
+      <SpeakersList
+        onLowerHand={lowerHand}
+        speakers={participants.filter(({ handRaised }) => handRaised)}
+      />
       <form action={addParticipant(true)}>
         <input
           value={nameInputValue}
-          onChange={(e) => setNameInputValue(e.target.value)}
+          onChange={(e) => onNameInputChange(e.target.value)}
         ></input>
-        <button value="submit">add</button>
-        <label>gnarf</label>
+        <button value="submit">Meldung</button>
+        <button formAction={addParticipant(false)}>+</button>
+        {nameInputError === "participant-already-exists" ? (
+          <label>gnarf</label>
+        ) : null}
       </form>
       <ul>
-        {participants.map(({ name }) => (
-          <li>{name}</li>
-        ))}
+        {participants
+          .filter(({ handRaised }) => !handRaised)
+          .map(({ name }) => (
+            <li key={name} onClick={() => raiseHand(name)}>
+              {name}
+            </li>
+          ))}
       </ul>
     </>
   );
